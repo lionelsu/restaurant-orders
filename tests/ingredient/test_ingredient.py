d37a2d344651c8234cfc6627f2b8137e299ff659
Lionel Miranda <bibliothecalionel@gmail.com>
2023-11-10 21:14:20 -0400

    tests: tests for Ingredient module

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("bacon")
+
+    assert (ingredient2.__eq__(ingredient1)) is False
+    assert (ingredient2.__eq__(ingredient2)) is True
+
+    assert ingredient1.__hash__() != ingredient2.__hash__()
+    assert ingredient1.__hash__() == ingredient1.__hash__()
+
+    assert ingredient1.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert ingredient1.name == "queijo mussarela"
+
+    assert len(ingredient1.restrictions) == 2

91a394dbd3bdef0699cc64b1e589038cb824ad5b
Lionel Miranda <bibliothecalionel@gmail.com>
2023-11-12 13:34:59 -0400

    tests: tests for Dish class

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,4 +3,7 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 # Req 2
 def test_dish():
-    pass
+    dish1 = Dish('X-Salada', 10.0)
+
+    assert dish1.name == 'X-Salada'
+    assert dish1.price == 10.0
